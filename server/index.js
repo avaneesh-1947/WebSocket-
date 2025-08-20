e8feb162bb4fb1ecdf14fec3ca0f5c8fef5aacbf
Rizwan <mdrizwan6386@gmail.com>
2025-08-21 00:20:28 +0530

    correcrtion in cors

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@
   
   const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:5173", "https://web-socket-lime-phi.vercel.app/"],
+        origin:"*",
         methods: ["GET", "POST"],
         credentials: true 
     }   
