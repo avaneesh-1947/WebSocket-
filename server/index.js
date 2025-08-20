8c7d3694ebddeafd3bbaea8f34b6133495a3b24c
Avaneesh <avaneesh.m.maurya@gmail.com>
2025-08-21 00:03:59 +0530

    bankend added

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@
   
   const io = new Server(server,{
     cors: {
-        origin: "http://localhost:5173", 
+        origin: ["http://localhost:5173", "https://web-socket-lime-phi.vercel.app/"],
         methods: ["GET", "POST"],
         credentials: true 
     }   
